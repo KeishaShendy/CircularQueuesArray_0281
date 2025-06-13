5f7e8bf673868d892569b1e4b30c279791fe6fbb
keishashendy <shendyarrafli7@gmail.com>
2025-06-13 09:23:17 +0700

    Menambahkan Pemeriksaan Kosong di Metode Display

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -65,3 +65,9 @@ public:
     void display() {
         int FRONT_position = FRONT;
         int REAR_position = REAR;
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
